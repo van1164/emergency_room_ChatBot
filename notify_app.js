02d90e0ec4790d4195554ae626c8d85963089cc8
EC2 Default User <ec2-user@ip-172-31-94-127.ec2.internal>
2022-11-29 10:22:48 +0000

    dotenv

--- notify_app.js
+++ notify_app.js
@@ -2,8 +2,9 @@
 const request = require('request');
 // 요청을 위한 상수를 선언합니다: TOKEN은 자신의 것을 입력해주세요.
 const TARGET_URL = 'https://notify-api.line.me/api/notify';
-const TOKEN = 'YK56BfHFgpILrxRk1FZrdcouFguf5CBA5qxM3zfDH6N9jR/cfPxVdK1P9vZHAk69mZDSlSkXNLy25pzqJbXrn3y76hwmH0Kiuvx3OadFYcuidWSp7VYAj4SqJSljv/q5KoYAOE2il8jmQkf4bQKvXQdB04t89/1O/w1cDnyilFU=';
-// 요청합니다.
+require("dotenv").config("/home/ec2-user/OSS/Experiments/messenger/chat/custom_modules/.env");
+// const KAKAO_KEY = process.env.KAKAO_KEY;
+const TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
 request.post(
     {
         url: TARGET_URL,
